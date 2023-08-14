ce371edd58fc98ae1863337b71e260bbe61503be
zdebra <zd.brabec@gmail.com>
2023-08-14 21:38:56 +0200

    tmp

--- kv-store/src/main.rs
+++ kv-store/src/main.rs
@@ -1,17 +1,94 @@
 use std::{
     collections::HashMap,
     io::{self, BufRead, Write},
+    time::Duration,
 };
 
 mod message;
+
+struct Node {
+    id: Option<usize>,
+    all_nodes: Vec<usize>,
+    receiving_dur: Duration,
+    executing_dur: Duration,
+}
+
+impl Node {
+    fn new() -> Self {
+        Self {
+            id: None,
+            all_nodes: Vec::new(),
+            receiving_dur: Duration::from_millis(500),
+            executing_dur: Duration::from_millis(500),
+        }
+    }
+
+    fn init(&mut self, id: usize, all_nodes: Vec<usize>) {
+        eprintln!("initializing node..");
+        self.id = Some(id);
+        self.all_nodes = all_nodes;
+    }
+
+    fn is_receiving(&self) -> bool {
+        let total_dur = self.receiving_dur + self.executing_dur;
+        todo!()
+    }
+
+    fn next_msg_id(&mut self) -> usize {
+        todo!()
+    }
+}
 
 fn main() {
     eprintln!("started receiving messages");
     let mut last_msg_id = 0;
     let mut kv = KVStore::new();
+    let mut txn_batcher = TxnBatcher::new();
+    let mut node = Node::new();
+    let mut receiving = true;
     loop {
+        if !node.is_receiving() && receiving {
+            eprintln!("broadcast phase started");
+            txn_batcher.stop_receiving();
+            init_broadcast(&mut node, &mut txn_batcher);
+            receiving = false;
+        }
+        if node.is_receiving() && !receiving {
+            eprintln!("receiving phase started");
+            txn_batcher.start_receiving();
+            receiving = true;
+        }
         let msg = receive_msg();
-        process_msg(msg, &mut last_msg_id, &mut kv);
+
+        // todo figure out how to sync broadcast messages
+
+        process_msg(msg, &mut last_msg_id, &mut kv, &mut txn_batcher, &mut node);
+    }
+}
+
+fn init_broadcast(node: &Node, txn_batcher: &mut TxnBatcher) {
+    eprintln!(
+        "node {} start broadcast for {} nodes",
+        node.id.unwrap(),
+        node.all_nodes.len()
+    );
+    let txns = txn_batcher.last_batch();
+    for neighbour in &node.all_nodes {
+        if *neighbour == node.id.expect("node is init") {
+            continue;
+        }
+
+        send_msg(message::Message::new_request(
+            node.id.expect("node is init").to_string(),
+            neighbour.to_string(),
+            node.next_msg_id(),
+            message::Payload::BroadcastTxn { txns: txns },
+        ));
+        eprintln!(
+            "broadcasted {} transactions for node {} done",
+            txns.len(),
+            *neighbour
+        )
     }
 }
 
@@ -42,41 +119,33 @@ fn send_reply(msg_id: usize, reply_to: message::Message, payload: message::Paylo
     });
 }
 
-fn process_msg(req: message::Message, last_msg_id: &mut usize, kv: &mut KVStore) {
+fn process_msg(
+    req: message::Message,
+    last_msg_id: &mut usize,
+    kv: &mut KVStore,
+    txn_batcher: &mut TxnBatcher,
+    node: &mut Node,
+) {
     *last_msg_id += 1;
     let next_msg_id = *last_msg_id;
 
-    let mut uncommitted_txns = Vec::new();
-    let mut cur_node: Option<usize> = None;
-    let mut next_tx_seq = 0;
-    let mut all_nodes = Vec::new();
+    let mut next_tx_seq = 0; // todo: move this to txn_batcher
 
     use message::Payload::*;
     match req.body.payload.clone() {
         Init { node_id, node_ids } => {
-            eprintln!("initializing node..");
-            cur_node = Some(node_id);
-            all_nodes = node_ids;
+            node.init(node_id, node_ids);
             send_reply(next_msg_id, req, message::Payload::InitOk);
         }
         Txn { txn } => {
-            uncommitted_txns.push(LocalTxn {
+            txn_batcher.push(LocalTxn {
                 seq: next_tx_seq.clone(),
-                origin: cur_node.expect("node was initiated"),
+                origin: node.id.expect("node was initiated"),
                 txn,
                 reply_to: req.src,
                 in_reply_to: req.body.msg_id.expect("message id is there"),
             });
             next_tx_seq += 1;
-            // let operations = parse_op_req(txn);
-            // let results = kv.apply(operations);
-            // send_reply(
-            //     next_msg_id,
-            //     req,
-            //     message::Payload::TxnOk {
-            //         txn: results_to_txn(results),
-            //     },
-            // )
         }
         BroadcastTxn { txns } => {
             // todo:    1. gather txns from all other nodes
@@ -89,6 +158,7 @@ fn process_msg(req: message::Message, last_msg_id: &mut usize, kv: &mut KVStore)
     }
 }
 
+#[derive(Debug, Clone)]
 struct LocalTxn {
     seq: usize,
     origin: usize,
@@ -187,5 +257,44 @@ impl KVStore {
                 }
             })
             .collect()
+    }
+}
+
+struct TxnBatcher {
+    batch: Vec<LocalTxn>,
+    receiving_stopped: bool,
+    queue: Vec<LocalTxn>,
+}
+
+impl TxnBatcher {
+    fn new() -> Self {
+        Self {
+            batch: Vec::new(),
+            receiving_stopped: false,
+            queue: Vec::new(),
+        }
+    }
+
+    fn push(&mut self, txn: LocalTxn) {
+        if self.receiving_stopped {
+            self.queue.push(txn);
+        } else {
+            self.batch.push(txn);
+        }
+    }
+
+    fn stop_receiving(&mut self) {
+        self.receiving_stopped = true;
+    }
+
+    fn start_receiving(&mut self) {
+        self.batch = self.queue.clone();
+        self.batch.clear();
+        self.queue.clear();
+        self.receiving_stopped = false;
+    }
+
+    fn last_batch(&mut self) -> Vec<LocalTxn> {
+        todo!()
     }
 }
